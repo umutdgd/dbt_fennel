5b06855baefd5d182780a325c162b97b4479a23e
umutdgd <umutdogdu@gmail.com>
2024-04-12 16:23:37 +0200

    dbt_forecast_prep2

--- models/prep/prep_forecast_day.sql
+++ models/prep/prep_forecast_day.sql
@@ -4,12 +4,12 @@ WITH forecast_day_data AS (
 ),
 add_features AS (
     SELECT *
-        ,DATE_PART('day_of_month',date) AS day_of_month -- day of month as a number
-        ,TO_CHAR('month_of_year',date) AS month_of_year -- month name as a text
-        ,DATE_PART('year',date) AS year -- year as a number
-        ,TO_CHAR('day_of_week',date) AS day_of_week -- weekday name as text
-        ,DATE_PART('week_of_year',date) AS week_of_year -- calender week number as number
-        ,TO_CHAR('year_and_week', date) AS year_and_week -- year-calenderweek as text like '2024-43'
+        ,DATE_PART('day_of_month',INT) AS day_of_month -- day of month as a number
+        ,TO_CHAR('month_of_year',VARCHAR) AS month_of_year -- month name as a text
+        ,DATE_PART('year',INT) AS year -- year as a number
+        ,TO_CHAR('day_of_week',VARCHAR) AS day_of_week -- weekday name as text
+        ,DATE_PART('week_of_year',INT) AS week_of_year -- calender week number as number
+        ,TO_CHAR('year_and_week', VARCHAR) AS year_and_week -- year-calenderweek as text like '2024-43'
 
     FROM forecast_day_data
 )
